7443c48857f66140461891387244ad3e06796e06
CorbinSpence <corbinspence16@gmail.com>
2023-04-04 22:56:05 -0400

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# SVG-Generator
+# SVG Generator
+
+![License](https://img.shields.io/badge/license-None-lightgrey)
+
+## Description
+
+This program is supposed to generate basic svg's based on user input. Users have the option of creating circles, squares, and triangles specifically. Users are also able to put a maximum of 3 letters in the shape and specify what colors they want for the text and shape.
+
+## Table of Contents
+
+1. [Installation](#Installation)
+2. [Usage](#Usage)
+3. [License](#License)
+4. [Contributing](#Contributing)
+5. [Tests](#Tests)
+6. [Questions](#Questions)
+
+## Installation
+
+You need to install node ver. 16.0.0 and the latest version of npm. You may also need to run "npm i" in the repo folder to install dependencies.
+
+## Usage
+
+First, you go to your terminal and change your directory to the repo folder. Then you type "node index.js" to run the code. You will be given 4 prompts that ask about the colors, text and shape of the desired svg. The text should be no greater than 3 characters. Color can be specified either by name or by hex code (e.g. #000000). Once everything is properly set up, a svg should appear under "assets" with a name that is generated based on your parameters.
+
+## License
+
+None
+
+## Contributing
+
+Corbin Spence
+
+## Tests
+
+To test this program, type "npm test" to run basic tests on the code.
+
+## Questions
+
+Github: [profile](https://github.com/CorbinSpence)
+
+Email: corbinspence16@gmail.com
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const inquirer = require('inquirer')
 const jest = require('jest')
 const fs = require('fs')
+const Square = require('./lib/square.js')
+const Circle = require('./lib/circle.js')
+const Triagle = require('./lib/triangle.js')
 const shapeDimensions = {
-    circle:'cx="50" cy="50" r="40"',
-    rect:'width="100" height="100"',
+    circle:'cx="50" cy="50" r="90"',
+    rect:'width="300" height="200"',
     ellipse:'cx="50" cy="50" rx="30" ry="40"'
 }
+const objects = {
+    circle: Circle,
+    square: Square,
+    triangle: Triagle,
+}
+var shapes = ['circle', 'triangle', 'square']
 
 inquirer.prompt([
     {
@@ -19,9 +28,10 @@ inquirer.prompt([
         message:'Enter text color:',
     },
     {
-        type:'input',
+        type:'list',
         name:'shape',
-        message:'Enter desired shape:',
+        message:'Choose desired shape:',
+        choices:[...shapes],
     },
     {
         type:'input',
@@ -29,12 +39,7 @@ inquirer.prompt([
         message:'Enter shape color:',
     },
 ]).then((response)=>{
-    var svgContent = 
-`
-<svg width="100" height="100">
-    <${response.shape} ${shapeDimensions[response.shape]} stroke="${response.shapeColor}" stroke-width="4" fill="${response.shapeColor}"/>
-    <text x="39" y="54" fill="${response.textColor}">${response.text}</text>
-</svg>
-`;
+    var shape = new objects[response.shape](response.shapeColor, response.text, response.textColor)
+    var svgContent = shape.render()
     fs.writeFile(`./assets/${response.shapeColor}${response.shape}_${response.textColor}text_${response.text}.svg`, svgContent, (err)=>{err? console.log('failure'):console.log('success')})
 })
